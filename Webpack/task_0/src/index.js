27f7a0d4ad4d0ae31c0651c3cc6a316dc3f992b9
ikerikks <ikerikks@gmail.com>
2024-07-22 14:01:36 +0200

    fix

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
 import $ from "jquery";
 
-const body = $('body');
-body.append(
+$('body').append(
   $('<p>').text('Holberton Dashboard'),
   $('<p>').text('Dashboard data for the students'),
   $('<p>').text('Copyright - Holberton School') 
