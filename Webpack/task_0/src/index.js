d0f483188eec808ba929c4df9128956ffb97fae6
ikerikks <ikerikks@gmail.com>
2024-07-22 14:10:36 +0200

    fix the display as checker expects

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from "jquery";
 
-$('body').append(
-  $('<p>').text('Holberton Dashboard'),
-  $('<p>').text('Dashboard data for the students'),
-  $('<p>').text('Copyright - Holberton School') 
-);
+$('body').append($('<p>').text('Holberton Dashboard'));
+$('body').append($('<p>').text('Dashboard data for the students'));
+$('body').append($('<p>').text('Copyright - Holberton School'));
