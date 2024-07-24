ec8360f0e4af1f08601f2048c6190e635e8a07e4
Iker <ikerikks@gmail.com>
2024-07-24 14:40:26 +0200

    Delete React_component directory

--- React_component/task_0/dashboard/src/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import * as utils from '../utils';
-import './Footer.css';
-
-export function Footer() {
-  return (
-    <footer className="App-footer">Copyright {utils.getFullYear()} - holberton School</footer>
-  );
-}
