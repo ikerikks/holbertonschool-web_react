fe721b36513bd48c62f1d979e71b420d6e927145
ikerikks <ikerikks@gmail.com>
2024-07-25 10:52:46 +0200

    config file

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+  ],
+};
