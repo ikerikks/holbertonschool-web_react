199853a49f6752dce82622788c030c8a1861de6f
ikerikks <ikerikks@gmail.com>
2024-08-01 20:25:18 +0200

    fix

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -8,51 +8,68 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import {getLatestNotification} from '../utils';
 
-export default class App extends React.Component {
-  static defaultProps = {
-    isLoggedIn: false
-  };
+ class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 }
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+      ]
+    };
 
-  static propTypes = {
-  isLoggedIn: PropTypes.bool
-}
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
 
   render() {
+    const { isLoggedIn } = this.props;
+    const { listCourses, listNotifications } = this.state;
+
     return (
       <>
-        <Notifications
-          listNotifications = {
-            [
-              {id: 1, type: 'default', value: 'New course available'},
-              {id: 2, type: 'urgent', value: 'New resume available'},
-              {id: 3, type: 'urgent', html: {__html: getLatestNotification()}},
-            ]
-          }
-        />
         <div className="App">
+          <Notifications listNotifications={listNotifications}/>
           <Header />
-          <main className="App-body">
-            {
-              this.props.isLoggedIn?
-                <CourseList 
-                  listCourses={
-                    [
-                      {id: 1, name: 'ES6', credit: '60'},
-                      {id: 2, name: 'Webpack', credit: '20'},
-                      {id: 3, name: 'React', credit: '40'}
-                    ]
-                  }
-                />:
-                <>
-                  <p>Login to access the full dashboard</p>
-                  <Login />
-                </>
-            }
-          </main>
+          <div className="App-body">
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
           <Footer />
         </div>
       </>
     );
-
   }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}
+};
+
+export default App;
