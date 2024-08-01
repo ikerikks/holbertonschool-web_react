67b9258fb50ef4b62bd1d688bc468c752b46c0e2
ikerikks <ikerikks@gmail.com>
2024-08-01 19:54:42 +0200

    fix

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -20,7 +20,7 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <Notifications displayDrawer={true}
+        <Notifications
           listNotifications = {
             [
               {id: 1, type: 'default', value: 'New course available'},
